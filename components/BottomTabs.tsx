1cdd4dfce3e92c69755a440c249449c8ffa7a4c5
seonew <seonew.888@gmail.com>
2023-06-27 17:28:47 +0900

    Modified `BottomTabs` component

--- components/BottomTabs.tsx
+++ components/BottomTabs.tsx
@@ -2,27 +2,22 @@
 
 import { MapPinIcon, MapIcon, StarIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 const BottomTabs = () => {
   const tabCSS = "w-full h-full flex items-center justify-center";
   const iconCSS = "w-5 h-5 m-auto";
   const activeCSS = "text-green-500 font-semibold";
-  const [active, setActive] = useState("1");
   const fixedCSS = "fixed w-full left-0";
-
-  const handleClickItem = (current) => () => {
-    setActive(current);
-  };
+  const pathname = usePathname();
 
   return (
     <div
       className={`${fixedCSS} flex items-center justify-between h-16 text-sm bottom-0 z-50 bg-white divide-x shadow-2xl`}
     >
       <Link
-        href="/"
-        className={`${tabCSS} ${active === "1" ? activeCSS : ""}`}
-        onClick={handleClickItem("1")}
+        href="/mypage"
+        className={`${tabCSS} ${"/mypage" === pathname ? activeCSS : ""}`}
       >
         <div>
           <MapPinIcon className={`${iconCSS}`} />
@@ -31,8 +26,7 @@ const BottomTabs = () => {
       </Link>
       <Link
         href="/"
-        className={`${tabCSS} ${active === "2" ? activeCSS : ""}`}
-        onClick={handleClickItem("2")}
+        className={`${tabCSS} ${"/" === pathname ? activeCSS : ""}`}
       >
         <div>
           <MapIcon className={`${iconCSS}`} />
@@ -41,8 +35,7 @@ const BottomTabs = () => {
       </Link>
       <Link
         href="/favorites"
-        className={`${tabCSS} ${active === "3" ? activeCSS : ""}`}
-        onClick={handleClickItem("3")}
+        className={`${tabCSS} ${"/favorites" === pathname ? activeCSS : ""}`}
       >
         <div>
           <StarIcon className={`${iconCSS}`} />
